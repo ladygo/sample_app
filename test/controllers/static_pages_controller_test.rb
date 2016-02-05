190eb649f9538b87127bae324219c97005456fbd
ladygo <halo-lee@qq.com>
2016-02-06 00:27:47 +0800

    Finish the exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	# cut the duplicate
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
 		#test the page title
-		assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+		assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
 		#test the page title
-		assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+		assert_select "title", "Help | #{@base_title}"
   end
 
 
@@ -21,7 +27,13 @@ class StaticPagesControllerTest < ActionController::TestCase
 		get :about
 		assert_response :success
 		#test the page title
-		assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+		assert_select "title", "About | #{@base_title}"
 	end
-
+	
+	# test case the contact page
+	test "should get contact" do
+		get :contact
+		assert_response :success
+		assert_select "title", "Contact | #{@base_title}"
+	end
 end
