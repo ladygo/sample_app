d9d0a6db6aade4445d7988a925af1d9ac39fd399
ladygo <halo-lee@qq.com>
2016-02-11 23:41:50 +0800

    Finished the sign up exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,11 +8,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "invalid signup information" do
 		get signup_path
 		assert_no_difference 'User.count' do
-			post users__path, user: { name: "",
+			post users_path, user: { name: "",
 															email: "user@invalid",
 															password: "foo",
 															password_confirmation: "bar" }
 		end
+		assert_template 'users/new'
+		assert_select 'div#<CSS id for error explanation>'
+		assert_select 'div.<CSS class for field with error>'
 	end
 
 	test "valid signup information" do
@@ -30,5 +33,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			}
 		end
 		assert_template 'users/show'
+		#assert_not flash.FILL_IN
 	end
 end
